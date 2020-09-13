509d6f41ee2d6a8a872af47104126a010c2f0d47
TollesDJones <49727120+TollesDJones@users.noreply.github.com>
2020-09-13 13:20:42 -0400

    Add Branching Example
    Added two examples for using if/then statements.

--- main.py
+++ main.py
@@ -27,12 +27,37 @@ The if statement
 Branching gives us the opportunity to execute or skip code based on a condition
 
 for example:
-if raining == "true"
-    do some special action
+if condition == "true"
+    do this
 """
+#Example 1 (shorthand notation)
+#Change the values of rain and sun to see different results
+
+rain = False
+sunny = False
+haveUmbrella = False
+
+if rain:
+    print("It is raining...", "\nGetting your umbrella ready.")
+    haveUmbrella = True
+    if haveUmbrella:
+        print("You have your umbrella handy!")
+        print("The value of haveUmbrella is: ", haveUmbrella)
+
+elif sunny:
+    print("Grab your sunglasses")
+    haveUmbrella = False
+    print("The value of haveUmbrella is: ", haveUmbrella)
+
+else:
+    print("It must be cloudy because the value of rain is {}".format(rain),
+          "and the value of sunny is {}".format(sunny))
 
 
-runs = 6
-for i in range(runs):
-    print(random.randrange(6)+1)
+#Example 2 (formatted using longhand)
+money = True
+if ( money != True):
+    print("i am broke!".title()) #Remember .title() is a string helper method
 
+elif (money == True):
+    print("i am rich!".upper()) #Remember .upper() is a builtin string helper method
